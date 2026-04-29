e12592d22745e3f5a2cb3d8dd902353e49986bf3
Julia Vister <juliavister@gmail.com>
2026-04-29 16:18:08 +0200

    update on status

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -4,15 +4,10 @@ export default function CurrentStatus({ className = "" }) {
   return (
     <section id="status" className={`section current-status ${className}`}>
       <div className="section-content fade-up">
-        <h2>Current Status: Revising</h2><br></br>
+        <h2>Current Status: Polishing</h2><br></br>
         <p>
-        As of April 2026, I have completed the first draft of my thesis. I am currently 
-        in the process of revising and refining the content based on feedback from my supervisor
-        and friends who include both cybersecurity experts and non-technical readers.
-        The next steps involve polishing the writing, ensuring the arguments are clear and well-supported, 
-        and preparing for the final submission. 
-        I am also working on creating a presentation to effectively communicate my findings for a defense in June 2026.
-
+        As of April 2026, I completed the first draft of my thesis. 
+        I am currently polishing the writin and preparing for the final submission. 
         </p>
         <p className="highlight">
           <strong>Next milestone:</strong> Delivery {" "}
@@ -20,9 +15,9 @@ export default function CurrentStatus({ className = "" }) {
         </p>
 
         <div className="status-meter">
-          <div className="status-fill" style={{ width: "87%" }}></div>
+          <div className="status-fill" style={{ width: "95%" }}></div>
         </div>
-        <p className="progress-text">Progress: 85% completed</p>
+        <p className="progress-text">Progress: 95% completed</p>
       </div>
 
       <a href="#gallery" className="scroll-down light">
